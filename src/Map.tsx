cc4b798bd126eb5c1dcc030199efa9c7cb2451d4
Weston Novelli <corridore407@gmail.com>
2022-12-20 12:10:51 -0600

    add map, favicon, cleanup

--- src/Map.tsx
+++ src/Map.tsx
@@ -1,7 +1,8 @@
 import React from "react";
+import MapSvg from "./MapSvg";
 
 const Map: React.FC = () => {
-  return <div>map will go here</div>
+  return <div className="w-screen flex justify-center bg-emerald-700"><MapSvg className="h-screen"/></div>
 };
 
 export default Map;
